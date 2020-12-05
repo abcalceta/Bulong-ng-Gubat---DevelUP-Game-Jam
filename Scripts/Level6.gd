eb6d7fc440f4c80fe35dc3161a73a240dfd618df
wakisalvador <69634333+wakisalvador@users.noreply.github.com>
2020-12-05 17:35:42 +0800

    Added new levels.

---
+++ Scripts/Level6.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().change_scene("res://Scenes/Level7.tscn")
+	pass # Replace with function body.
+
+func _on_Area2D2_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().change_scene("res://Minigames/scenes/GameManager.tscn")
+	pass # Replace with function body.
+
+func _on_DEATH_body_entered(body):
+	if body.is_in_group("player"):
+		get_tree().reload_current_scene()
+	pass # Replace with function body.
